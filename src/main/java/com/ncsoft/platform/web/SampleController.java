0d02a6f6265ee8cba46fca180844c49a16300b16
HeeJu Lee <leeheeju@gmail.com>
2016-05-24 00:03:47 +0900

    Json 사용하기

--- src/main/java/com/ncsoft/platform/web/SampleController.java
+++ src/main/java/com/ncsoft/platform/web/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ncsoft.platform.domain.ProductVO;
@@ -61,5 +62,13 @@ public class SampleController {
 	public void doF(String msg) {
 		
 		logger.info("doF called .... " + msg);
+	}
+	
+	@RequestMapping("/doJson")
+	public @ResponseBody ProductVO doJson() {
+		
+		ProductVO product = new ProductVO("sample", 100);
+		
+		return product;
 	}
 }
