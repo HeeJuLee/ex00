cb902b52feb322078f8621b300ad2e044740444f
HeeJu Lee <leeheeju@gmail.com>
2016-05-23 23:11:30 +0900

    Spring Project 기본 구성, MyBatis 설정

---
+++ src/test/java/com/ncsoft/platform/MySqlConnectionTest.java
@@ -0,0 +1,28 @@
+package com.ncsoft.platform;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySqlConnectionTest {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/studydb";
+	private static final String USER = "study";
+	private static final String PASSWORD = "study";
+
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+			
+			System.out.println(con);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
